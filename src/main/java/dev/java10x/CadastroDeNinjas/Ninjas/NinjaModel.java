e6c181949f22ec69d6f8406a2fab35eb9a8c293e
Marcos Antonio Fonseca da Costa <marcos.costa@ham.net>
2026-07-01 15:47:56 -0300

    fix: Inclui a coluna de rank na minha entidade, porém já estava incluida na migration V2

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -31,6 +31,9 @@ public class NinjaModel {
     @Column (name = "imgUrl")
     private String imgUrl;
 
+    @Column (name = "rank")
+    private String rank;
+
     @Column (name = "idade")
     private int idade;
 
