3f882a3a406b55b33d7b2f260d84db935699c2d4
Marcos Antonio Fonseca da Costa <marcos.costa@ham.net>
2026-07-14 17:10:57 -0300

    feat: Incluido rota de deleção na pagina para lista ninjas e incrementado css.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -1,8 +1,11 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,4 +26,16 @@ public class NinjaControllerUI {
         return "listarNinjas";
     }
 
+
+    // Deletar Ninja (DELETE)
+    @GetMapping("/deletar/{id}")
+    public String deletarNinjaPorId(@PathVariable Long id){
+        ninjaService.deletarNinjaPorId(id);
+        return "redirect:/ninjas/ui/listar";
+
+        }
+
+
 }
+
+
