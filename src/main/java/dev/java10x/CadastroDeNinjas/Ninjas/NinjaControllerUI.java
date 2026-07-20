783e7fff0c0c4e675f83d9598cb74542184ab8fa
Marcos Antonio Fonseca da Costa <marcos.costa@ham.net>
2026-07-20 16:27:56 -0300

    feat: Implementa rota para detalhar ninja e adicionar ninjas e inclui botoes no css para execução das rotas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -1,12 +1,11 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -34,6 +33,34 @@ public class NinjaControllerUI {
         return "redirect:/ninjas/ui/listar";
 
         }
+
+    // Mostrar Ninja por ID (READ)
+    @GetMapping("/listar/{id}")
+    public String listarNinjasPorId(@PathVariable Long id, Model model){
+        NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
+        if(ninja != null ) {
+            model.addAttribute("ninja",ninja);
+            return "detalhesNinjas";
+        }else {
+            model.addAttribute("mensagem","Ninja não encontrado");
+            return "listarNinjas";
+        }
+
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarNinja(Model model) {
+        model.addAttribute("ninja", new NinjaDTO());
+        return "adicionarNinja";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes) {
+        ninjaService.criarNinja(ninja);
+        redirectAttributes.addFlashAttribute("mensagem", "Ninja cadastrado com sucesso!");
+        return "redirect:/ninjas/ui/listar";
+    }
+
 
 
 }
